1fe033b414d22161e19c39d174c947b0e5e93f19
Oussama <oussamaazrour@gmail.com>
2025-02-07 19:25:58 +0100

    Log Out

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -95,4 +95,24 @@ export class AuthService {
     );
   }
 
+  logout(): void {
+    localStorage.removeItem('currentUser');
+    localStorage.removeItem('isAuthenticated');
+  }
+
+  isCollector(): boolean {
+
+    const userString = localStorage.getItem('currentUser');
+    if (!userString) {
+      return false;
+    }
+    try {
+      const user: User = JSON.parse(userString);
+      return user.role ==='collector';
+    } catch (error) {
+      console.error('Error parsing user from localStorage:', error);
+      return false;
+    }
+  }
+
 }
